b6b1ec1bdb43af351c34dd9ec5842328312a1621
milan1986 <milan1986@gmail.com>
2026-09-25 23:32:42 +0500

    add startup connectivity check for database

--- charityevents-api/event_db.js
+++ charityevents-api/event_db.js
@@ -14,4 +14,13 @@ const connectionPool = mysql.createPool({
 
 const dbPool = connectionPool.promise();
 
+connectionPool.getConnection((connectionError, connection) => {
+    if (connectionError) {
+        console.error('charityevents_db connection failed:', connectionError.message);
+        return;
+    }
+    console.log('charityevents_db is connected and ready.');
+    connection.release();
+});
+
 module.exports = dbPool;
